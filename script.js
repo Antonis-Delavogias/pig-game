0a287ed4aaa4ea3ceec7f209b3bab2d1f66625af
AntonisDela <antonisdelavogias@icloud.com>
2024-02-21 11:08:46 +0200

    selecting elements

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const diceNew = document.querySelector("btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
