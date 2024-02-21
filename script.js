c0cca8c888febd44b187fa419e967dcc8a4ead9f
AntonisDela <antonisdelavogias@icloud.com>
2024-02-21 12:05:11 +0200

    implented the hold button and made the switchPlayer lines of code into a function

--- script.js
+++ script.js
@@ -20,6 +20,15 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+//Functions
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
 //Rolling dice functionality
 btnRoll.addEventListener("click", function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -37,10 +46,32 @@ btnRoll.addEventListener("click", function () {
   } else {
     //switch to next pl
     currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener("click", function () {
+  //add current score to active player's score
+  scores[activePlayer] += currentScore;
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // check if player's score is >= 100
+  if (scores[activePlayer] >= 100) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
+    btnRoll.disabled = true;
+    btnHold.disabled = true;
+  } else {
+    switchPlayer();
+  }
+
+  //Finish the game
+
+  //Switch to the next player
 });
